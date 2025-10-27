34dd82b66ff9b7fde93b82c4960a1fecf9b19563
Guilherme Borges de Gouveia <guilhermeborgesgv@gmail.com>
2025-10-27 16:31:23 -0300

    Enhance temperature conversion options
    Added functionality to convert temperatures from Fahrenheit to Celsius and vice versa.

--- CelciusForFahrenheit.c
+++ CelciusForFahrenheit.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
-void limpar_entrada()
-{
+void limpar_entrada(){
     char c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
+
 int main()
 {
+
     double C, F;
     char resp;
-    do
-    {
+    char R;
+
+    do {
+
+    printf("Caso queira de Celsius p Fahrenheit escreva C, se for de Fahrenheit p Celsius escreva F: ");
+    scanf(" %c", &R);
+
+    if (R == 'C'|| R == 'c' ) {
         printf("Digite a temperatura em Celsius: ");
         scanf("%lf", &C);
         F = 9.0 * C / 5.0 + 32.0;
         printf("Equivalente em Fahrenheit: %.1lf\n", F);
-        printf("Deseja repetir (s/n)? ");
-        limpar_entrada();
-        scanf("%c", &resp);
     }
-    while (resp == 's');
+
+    else if (R == 'F'|| R == 'f') {
+        printf("Digite a temperatura em Fahrenheit: ");
+        scanf("%lf", &F);
+        C = 5.0 * (F - 32.0) / 9.0;
+        printf("Equivalente em Celsius: %.1lf\n", C);
+    }
+
+    else {
+        printf("Entre somente com F/C");
+    }
+
+        printf("Deseja repetir (s/n)? ");
+        scanf(" %c", &resp);
+    } while (resp == 's'|| resp == 'S');
+
     return 0;
 }
+
+
 
